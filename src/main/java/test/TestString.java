2bbf65ba322725e5c90873ca0f85eaa75129134f
dogNoPig <3597906135@qq.com>
2020-09-17 09:52:33 +0800

    20200917-对字符串的操作进阶

--- src/main/java/test/TestString.java
+++ src/main/java/test/TestString.java
@@ -9,8 +9,12 @@ package test;
 public class TestString {
     public static void main(String[] args) {
         String str=",111,79,1,0";
-        System.out.println(str.indexOf(","));
-        System.out.println(sub(str,str.indexOf(",")+1,str.length() - 7));
+
+//        System.out.println(str.indexOf(","));
+//        System.out.println(sub(str,str.indexOf(",")+1,str.length() - 7));
+        // 截取从1到第2个逗号的位置
+        String a1 = str.substring(1,str.indexOf(",",str.indexOf(",")+1));
+        System.out.println(a1);
     }
 
     public static String sub(String str, int begin, int end){
